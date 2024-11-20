3519f7647c372dacd7f5413881c52f51ec0ee17c
KEELANMURPHY-STUDENT <g00435965@atu.ie>
2024-11-20 16:56:47 +0000

    Task 1 complete

---
+++ src/main/java/ie/atu/week8/Customer.java
@@ -0,0 +1,40 @@
+package ie.atu.week8;
+
+public class Customer extends Person
+{
+    private String cust_num;
+    private boolean mailingList;
+
+    public Customer() {
+        super();
+    }
+
+    public Customer(String name, String address, String phone, String cust_num, boolean mailingList) {
+        super(name, address, phone);
+        this.cust_num = cust_num;
+    }
+
+    public String getCust_num() {
+        return cust_num;
+    }
+
+    public void setCust_num(String cust_num) {
+        this.cust_num = cust_num;
+    }
+
+    public boolean isMailingList() {
+        return mailingList;
+    }
+
+    public void setMailingList(boolean mailingList) {
+        this.mailingList = mailingList;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " " + "Customer" +
+                "cust_num='" + cust_num + '\'' +
+                ", mailingList=" + mailingList +
+                '}';
+    }
+}
---
+++ src/main/java/ie/atu/week8/CustomerApp.java
@@ -0,0 +1,9 @@
+package ie.atu.week8;
+
+public class CustomerApp {
+
+    public static void main(String[] args) {
+        Customer newCust = new Customer("Keelan","ATU", "34567","ATU123", false);
+        System.out.println(newCust.toString());
+    }
+}
